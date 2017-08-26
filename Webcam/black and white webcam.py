7ade71e9cc770f7d35ee192d9eb854325a49dd23
Tanay Toshniwal <tanaytoshniwal98@gmail.com>
2017-08-27 04:43:55 +0530

    display webcam in bnw using opencv python

---
+++ Webcam/black and white webcam.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy
+cap=cv2.VideoCapture(0)
+while True:
+	ret,frame=cap.read()
+	frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+	cv2.imshow('webcam',frame)
+	c=cv2.waitKey(1)
+	if c==27:
+		break
+cap.release()
+cv2.destroyAllWindows()
