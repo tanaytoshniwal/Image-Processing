4fc348a50fb2bb5e3fc5bc83ef5feeaffe8d1929
Tanay Toshniwal <tanaytoshniwal98@gmail.com>
2017-08-27 04:45:58 +0530

    edge detection program using opencv python

---
+++ Edge-Detection/edge.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2
+cap = cv2.VideoCapture(0)
+while True:
+	ret, frame = cap.read()
+	cv2.imshow('Edges',cv2.Canny(frame,130,210))
+	if cv2.waitKey(5) == 27:
+        	cap.release()
+		cv2.destroyAllWindows()
+		break
