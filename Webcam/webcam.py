0f9fca3b89067fc74eebd6443f42eff363955d8c
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-10-10 01:15:54 +0530

    Update webcam.py

--- Webcam/webcam.py
+++ Webcam/webcam.py
@@ -1,11 +1,10 @@
 import cv2
-import numpy
 cap=cv2.VideoCapture(0)
 while True:
 	ret,frame=cap.read()	
 	cv2.imshow('webcam',frame)
 	c=cv2.waitKey(1)
 	if c==27:
+		cap.release()
+		cv2.destroyAllWindows()
 		break
-cap.release()
-cv2.destroyAllWindows()
