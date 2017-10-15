e49dacb85ee10e6155bb0ad700bdc910e630834d
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-10-15 18:02:33 +0530

    Update circle_detection(HoughCircles).py

--- Circle-Detection/circle_detection(HoughCircles).py
+++ Circle-Detection/circle_detection(HoughCircles).py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 cap = cv2.VideoCapture(0)
-while True:
+while cap.isOpened():
     _,frame= cap.read()
     frame= cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     obj = cv2.HoughCircles(frame,cv2.cv.CV_HOUGH_GRADIENT,1, 260, param1=30, param2=65, minRadius=0, maxRadius=0)
